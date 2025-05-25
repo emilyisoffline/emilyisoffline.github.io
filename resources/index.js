59289bd6e6404cca1c3623bda9cf0a30f1b8ece3
emilyisoffline <emilypoletti5@gmail.com>
2025-05-26 01:20:51 +1000

    wow updated

--- resources/index.js
+++ resources/index.js
@@ -1,19 +1,19 @@
-document.body.addEventListener("click", function() {
-    let splash = document.getElementById("splash");
-    let mainContent = document.getElementById("mainContent");
-    let footer = document.querySelector("footer");
+// document.body.addEventListener("click", function() {
+//     let splash = document.getElementById("splash");
+//     let mainContent = document.getElementById("mainContent");
+//     let footer = document.querySelector("footer");
 
-    splash.classList.add("fade-out"); // Fade out splash screen
-    setTimeout(() => {
-        splash.style.display = "none"; // Hide it completely
-        mainContent.style.display = "block";
-        footer.style.opacity = "1"; // Fade in footer
-        setTimeout(() => {
-            mainContent.style.opacity = "1"; // Fade in main content
-        }, 50);
-    }, 1500);
+//     splash.classList.add("fade-out"); // Fade out splash screen
+//     setTimeout(() => {
+//         splash.style.display = "none"; // Hide it completely
+//         mainContent.style.display = "block";
+//         footer.style.opacity = "1"; // Fade in footer
+//         setTimeout(() => {
+//             mainContent.style.opacity = "1"; // Fade in main content
+//         }, 50);
+//     }, 1500);
 
-}, { once: true }); // Ensures it only runs once
+// }, { once: true }); // Ensures it only runs once
 
 const updatetimeElement = document.getElementById("updatetime");
 fetch("https://api.github.com/repos/emilyisoffline/emilyisoffline.github.io/branches/main")
